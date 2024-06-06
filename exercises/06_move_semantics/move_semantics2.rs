9e624d6584765514204bb908a231604752a6c804
Lucca Nunes <lucca.nunes@ifood.com.br>
2024-06-06 15:54:26 -0300

    fix conflit

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -8,12 +8,7 @@
 #[test]
 fn main() {
     let vec0 = vec![22, 44, 66];
-
-<<<<<<< HEAD:exercises/06_move_semantics/move_semantics2.rs
-    let vec1 = fill_vec(vec0);
-=======
     let mut vec1 = fill_vec(vec0.clone());
->>>>>>> tmp:exercises/move_semantics/move_semantics2.rs
 
     assert_eq!(vec0, vec![22, 44, 66]);
     assert_eq!(vec1, vec![22, 44, 66, 88]);
